cacbec1da788b8de3865b7db0e1053da82523505
Artem Balan <a.s.balan96@gmail.com>
2024-04-08 22:37:58 +0300

    simple greeting service

--- src/main/java/org/artem/balan/HelloResource.java
+++ src/main/java/org/artem/balan/HelloResource.java
@@ -1,15 +1,29 @@
 package org.artem.balan;
 
+import jakarta.inject.Inject;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
+import org.artem.balan.services.HelloService;
 
 @Path("/hello")
 public class HelloResource {
+
+    @Inject
+    HelloService helloService;
+
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
         return "Hello from Quarkus REST";
+    }
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("/polite/{name}")
+    public String greeting(@PathParam("name") String name) {
+        return helloService.politeHello(name);
     }
 }
